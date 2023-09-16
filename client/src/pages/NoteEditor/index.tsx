6e3137b7ec006cfa23e915f779e3cd4d796b3704
vedaant-varshney <vedaant.varshney@gmail.com>
2023-09-16 11:36:53 -0400

    Added node editor linked list

--- client/src/pages/NoteEditor/index.tsx
+++ client/src/pages/NoteEditor/index.tsx
@@ -1,48 +1,75 @@
 import NavigationBar from "@/components/Navigation/Navigation";
-import { Container, TextField } from "@mui/material";
-import { useState } from "react";
+import classnames from 'classnames';
+import { Container, Grid, TextField, TextareaAutosize } from "@mui/material";
+import { useEffect, useState } from "react";
+import { LinkedNode, ParagraphData } from "@/types/linkedlist.type";
+
+import styles from './NoteEditor.module.scss'
+
+
 
 function NoteEditor() {
-  const [editorContent, setEditorContent] = useState([{ key: 0, text: "" }]);
+  const [idCounter, setIdCounter] = useState(0)
+  const [editorContent, setEditorContent] = useState([{ key: 0, text: "", paragraphId: `para${idCounter}`, before: "", after: "" }]);
+
+
   const handleKeyPress = (event: any) => {
     if (event.key === "Enter") {
+      window.alert(event.target.id)
+      const length = editorContent.length
+      const curr = editorContent.slice(length - 1)[0]
+      curr.after = `para${idCounter + 1}`
       setEditorContent([
-        ...editorContent,
-        { key: editorContent.length, text: "" },
+        ...(editorContent.slice(0, length - 1)),
+        curr,
+        { key: editorContent.length, text: "", paragraphId: `para${idCounter + 1}`, before: "", after: "" },
       ]);
+      setIdCounter(idCounter + 1)
       console.log("enter press here! ");
     }
   };
+
+
   return (
-    <>
+    <div className={styles.NoteEditor}>
       <NavigationBar />
       <Container>
         <h1>Note Editor</h1>
         <form>
-          {editorContent.map((el) => (
-            <textarea
-              autoFocus
-              key={el.key}
-              value={el.text}
-              onChange={(e) => {
-                let editorCont = editorContent.map((editor, index) => {
-                  if (index !== el.key) {
-                    return editor
-                  }
-                  else {
-                    return {key: el.key, text: e.target.value}
-                  }
+          <Grid>
 
-                })
-                console.log(editorContent)
-                setEditorContent(editorCont)
-              }}
-              onKeyDown={handleKeyPress}
-            />
+          </Grid>
+
+          {editorContent.map((el) => (
+            <Grid item xs={12}>
+              <TextareaAutosize
+                className={styles.paragraph}
+                autoFocus
+                id={el.paragraphId}
+                key={el.key}
+                value={el.text}
+                onChange={(e) => {
+                  let editorCont = editorContent.map((editor, index) => {
+                    if (index !== el.key) {
+                      return editor
+                    }
+                    else {
+                      return { key: el.key, text: e.target.value, paragraphId: el.paragraphId, before: el.before, after: el.after }
+                    }
+
+                  })
+                  console.log(editorContent)
+                  setEditorContent(editorCont)
+                }}
+                onKeyDown={handleKeyPress}
+              />
+
+            </Grid>
           ))}
         </form>
+
       </Container>
-    </>
+      </div>
   );
 }
 
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+
+      <Head>
+        <script type="text/javascript" src="/static/textarea.js"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
