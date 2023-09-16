10f419645f4f28ac5c04af5a7fa13b6b5128698b
Brian <bzimmerman@csumb.edu>
2023-09-16 18:07:43 -0400

    added flask cors for cross-origin development

--- notenexus-lm-api/src/index.py
+++ notenexus-lm-api/src/index.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from lm import encode_text
 from db.model import Paragraph
 from db import (
@@ -10,6 +11,7 @@ from db import (
 )
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
