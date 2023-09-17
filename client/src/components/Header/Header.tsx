f35422c5ed1f417c22f7af8c5bf8b2f8123c223a
Delbert <delbert3luo@gmail.com>
2023-09-17 00:00:48 -0400

    styles for headeR

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import * as React from "react";
 import styles from "./Header.module.scss";
+import { usePathname } from "next/navigation";
 
 interface IHeaderProps {
   isNotLoggedIn?: boolean;
 }
 function Header({ isNotLoggedIn }: IHeaderProps) {
+  const pathname = usePathname();
   const allowedPages = isNotLoggedIn
     ? [{ name: "Home", href: "/" }]
     : [
@@ -17,15 +19,22 @@ function Header({ isNotLoggedIn }: IHeaderProps) {
   return (
     <div className={styles.Header}>
       <div className={styles.Header__navigationBar}>
-        {allowedPages.map((page) => (
-          <Link
-            className={styles.Header__navigationBar__item}
-            href={page.href}
-            key={page.name}
-          >
-            {page.name}
-          </Link>
-        ))}
+        {allowedPages.map((page) => {
+          const isActive = pathname.startsWith(page.href);
+          return (
+            <Link
+              className={
+                styles[
+                  `Header__navigationBar__item${isActive ? "--active" : ""}`
+                ]
+              }
+              href={page.href}
+              key={page.name}
+            >
+              {page.name}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
