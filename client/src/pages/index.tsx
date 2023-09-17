bfe26d4d84d0db19ed4257063666d921ffd25054
Delbert <delbert3luo@gmail.com>
2023-09-17 06:10:57 -0400

    wip

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from "../styles/Home.module.scss";
 import { useEffect } from "react";
 import router from "next/router";
 import { CircularProgress } from "@mui/material";
+import logo from "../assets/temporaryLogo.png";
+import Image from "next/image";
 
 export default function Home() {
   const { user } = useUser();
@@ -21,12 +23,35 @@ export default function Home() {
         <div className={styles.Home__container}>
           <SignIn />
           <div className={styles.Home__right}>
+            <Image
+              className={styles.Home__right__logo}
+              src={logo}
+              alt="logo"
+              height={160}
+              width={160}
+            />
+
             <h1 className={styles.Home__right__title}>
-              The best way to write your notes!
+              Revolutionizing the way you take notes
             </h1>
-            <p className={styles.Home__right__description}>
-              Use NoteNexus to instantly create links between your notes
-            </p>
+            <h3 className={styles.Home__right__subtitle}>
+              Smarter notes with less work. Unlock Insights with NoteNexus
+              effortlessly
+            </h3>
+            {/* <ul className={styles.Home__right__list}>
+              <li className={styles.Home__right__list__item}>
+                - Automatic note linking using large language models.
+              </li>
+              <li className={styles.Home__right__list__item}>
+                - Insight discovery through automatic note insights.
+              </li>
+              <li className={styles.Home__right__list__item}>
+                - Easy-to-use interface for note tracking and management.
+              </li>
+              <li className={styles.Home__right__list__item}>
+                - Paragraph-level organization and linking.
+              </li>
+            </ul> */}
           </div>
         </div>
       </div>
